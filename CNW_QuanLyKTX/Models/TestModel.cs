6fd53577eb2adbd7f646819394b1068d3880b031
Hoang Phuc <hphuc54543@gmail.com>
2020-11-29 10:20:14 +0700

    Create TestModel.cs

---
+++ CNW_QuanLyKTX/Models/TestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CNW_QuanLyKTX.Models
+{
+    public class TestModel
+    {
+        // ok
+    }
+}
