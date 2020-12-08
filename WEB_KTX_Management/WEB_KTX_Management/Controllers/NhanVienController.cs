7aeb1893447730a467ff2dc9aa54d4c98aed71bc
Hoang Phuc <hphuc54543@gmail.com>
2020-12-08 20:22:41 +0700

    test get api

--- WEB_KTX_Management/WEB_KTX_Management/Controllers/NhanVienController.cs
+++ WEB_KTX_Management/WEB_KTX_Management/Controllers/NhanVienController.cs
@@ -1,27 +1,29 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using WEB_KTX_Management.Models;
-using Newtonsoft.Json;
+using WEB_KTX_Management.Response;
 
 namespace WEB_KTX_Management.Controllers
 {
     public class NhanVienController : Controller
     {
-        //
-        // GET: /NhanVien/
-
-        private async Task<NhanVien> GetListNhanVien() {
-            return null;
-        }
-
         public ActionResult ListNhanVien()
         {
-            return View();
+            List<SinhVienResponse> list = new List<SinhVienResponse>();
+            using (var client = new WebClient()) {
+                var query = HttpUtility.ParseQueryString(string.Empty);
+                var url = new UriBuilder("http://192.168.1.58:5000/api/sinhvien");
+                url.Query = query.ToString();
+                string json = client.DownloadString(url.ToString());
+                SinhVienResponse sv = JsonConvert.DeserializeObject<SinhVienResponse>(json);
+                return View(sv);
+            }
         }
-
     }
 }
